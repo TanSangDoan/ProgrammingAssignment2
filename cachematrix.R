076b8aef87463d367e5b4c3af22656e89d54f947
TanSangDoan <doantansang0503@gmail.com>
2015-08-16 16:04:37 +0700

    Re-format

--- cachematrix.R
+++ cachematrix.R
@@ -7,27 +7,37 @@ makeCacheMatrix <- function(x = matrix()) {
     x <<- y
     m <<- NULL ## store matrix in cache
   }
-  get <- function() x ## get matrix
-  setInverse <- function(solve) m <<- solve ## set the inverse matrix
-  getInverse <- function() m ## get the inverse matrix
-  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse) ## create a list of functions
+  
+  get <- function()
+    x ## get matrix
+  setInverse <-
+    function(solve)
+      m <<- solve ## set the inverse matrix
+  getInverse <- function()
+    m ## get the inverse matrix
+  list(
+    set = set, get = get, setInverse = setInverse, getInverse = getInverse
+  ) ## create a list of functions
 }
 
 ## cacheSolve take a custom matrix type created by the makeCacheMatrix function
 ## and calculates the inverse matrix of it
 ## but first it checks to see if the calculation has been done before
-## if it has been done before it recalls the data from the cache. If it has not been done 
+## if it has been done before it recalls the data from the cache. If it has not been done
 ## before it calculates the inverse matrix then store it in the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
   m <- x$getInverse() ## query the matrix's cache
-  if(!is.null(m)) {  ## if there is a cache the inverse has been previously calculated
+  if (!is.null(m)) {
+    ## if there is a cache the inverse has been previously calculated
     message("getting cached data")  ## sent message indicating this is just cache
     return(m)
     
   }
-  data <- x$get()  ## get the matrix used by makeCacheMatrix function
-  m <- solve(data,...)  ## calculate the inverse of the matrix 
+  
+  data <-
+    x$get()  ## get the matrix used by makeCacheMatrix function
+  m <- solve(data,...)  ## calculate the inverse of the matrix
   x$setInverse(m)  ## store the inverse matrix in cache using the makeCacheMatrix set function
 }
